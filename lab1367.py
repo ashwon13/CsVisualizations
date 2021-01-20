b23b79f44b64c642108a2458c295baf92be768bd
ashwon13 <57849857+ashwon13@users.noreply.github.com>
2021-01-20 15:26:49 -0500

    Update lab1367.py

--- lab1367.py
+++ lab1367.py
@@ -12,65 +12,43 @@ cnvs.pack()
 
 data=open("lab1366.txt").read().split()
 xy=[]
-xypFirst=[]
+xyp=[]
 xypSecond=[]
 position=1
 x=0
 y=0
 switch="ready"
 stateDone="ready"
+position=0
 while position<len(data):
-   
-   try:
-      #Getting Data
-      x=float(data[position])
-      position+=1
-
-      
-   except:
-      stringValue=(data[position])
-      print stringValue
-      position+=1
-      if stringValue=="END_FILE":
-         break
-      #turns on the switch so that we know if we should start a new polly
-      elif stringValue=="END_ONE_POLY":
-         switch="on"
-      #turns stateDone to true so we can move onto next state
-      elif stringValue=="END_ALL_POLY":
-         stateDone="True"
-      #sets stateDone to false when we are not end all poly
-      elif stringValue != "END_ALL_POLY":
-         stateDone="false"
-      #turns off the switch so that we know if we should not start a new polly
-      elif stringValue !="END_One_POLY":
-         stateDone="false"
-   
-   try:
-      #Getting Data
-      y=float(data[position])
-      position+=1
-
-      xy.append((x,y))
-
-   except:
-      stringValue=(data[position])
-      print stringValue
-      position+=1
-      if stringValue=="END_FILE":
-         break
-      #turns on the switch so that we know if we should start a new polly
-      elif stringValue=="END_ONE_POLY":
-         switch="on"
-      #turns stateDone to true so we can move onto next state
-      elif stringValue=="END_ALL_POLY":
-         stateDone="True"
-      #sets stateDone to false when we are not end all poly
-      elif stringValue !="END_ALL_POLY":
-         stateDone="false"
-      #turns off the switch so that we know if we should not start a new polly
-      elif stringValue !="END_One_POLY":
-         stateDone="false"
   
+      
+      
+  if data[position]=="END_ALL_POLY":
+      position+=1
+      break 
+   
+  elif data[position]=="END_ONE_POLY":
+      position+=1
+      xy.append("END_ONE_POLY")
+  elif data[position]=="END_FILE":
+      break
+      
+  else:
+       try:
+         x=float(data[position])
+   
+         position+=1
+         y=float(data[position])
+         position+=1
+   
+         xy.append((x,y))   
+         
+        except:
+            position+=1
+         
+         
+   
 print xy
+   
    
